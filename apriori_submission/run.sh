36842434bb6e903730c15abe8128deaa553284eb
mahajandiwakar <dm3084@columbia.edu>
2015-04-27 17:06:25 -0400

    deleting extra files

--- apriori_submission/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash bash
-
-python src/apriori.py "${1}" "${2}" "${3}"
-
-# to run the apriori algorithm
-# sample usage: sh run.sh data/data.csv 0.3 0.5
-# sample  run: python src/apriori.py data/data.csv 0.3 0.5
